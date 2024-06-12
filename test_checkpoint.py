eaa34c4b615257aff9948f92e190f53c3c185bf0
simonwsy <wangshengyang2004@163.com>
2024-06-12 10:31:42 +0800

    fix minor bugs

--- test_checkpoint.py
+++ test_checkpoint.py
@@ -1,4 +1,4 @@
-#TODO: Add a maximum testing time constraint for each test, seperate the Omniverse output and the main program output
+# TODO: Add a maximum testing time constraint for each test, seperate the Omniverse output and the main program output
 import subprocess
 import sys
 import hydra
@@ -14,6 +14,7 @@ if platform == "darwin":
     logger.error("macOS is not supported! Exiting...")
     exit()
 config_name = "config_linux" if platform == "linux" else "config_windows"
+
 
 @hydra.main(config_path="./config", config_name=config_name, version_base="1.3")
 def main(cfg: DictConfig):
@@ -34,11 +35,11 @@ def main(cfg: DictConfig):
     CHECKPOINT_NAME = cfg.checkpoint_name
     RECORD_VIDEO = cfg.record_video
     RECORD_BASE_DIR = cfg.record_base_dir
-    test=True
-    num_envs=64
+    test = True
+    num_envs = 64
     if platform == "win32":
         driver = cfg.gym.omniisaacsimpathenv.split(":")[0]
-        command = f"{driver}: & cd {ISAAC_ROOT_DIR} & {PYTHON_PATH} {SCRIPTS_DIR} task={TASK_NAME} test={test} num_envs={num_envs}"    
+        command = f"{driver}: & cd {ISAAC_ROOT_DIR} & {PYTHON_PATH} {SCRIPTS_DIR} task={TASK_NAME} test={test} num_envs={num_envs}"
     elif platform == "linux":
         command = f"cd {ISAAC_ROOT_DIR} && {PYTHON_PATH} {SCRIPTS_DIR} task={TASK_NAME} test={test} num_envs={num_envs}"
     else:
@@ -46,37 +47,71 @@ def main(cfg: DictConfig):
         exit()
 
     if RESULT_DIR == "latest":
-            RESULT_DIR = sorted([x for x in os.listdir("results") if os.path.isdir(os.path.join("results", x))])[-1]
+        RESULT_DIR = sorted(
+            [
+                x
+                for x in os.listdir("results")
+                if os.path.isdir(os.path.join("results", x))
+            ]
+        )[-1]
     RESULT_DIR = f"results/{RESULT_DIR}"
-    
+
     def run_one(result_dir, iter, idx, command):
-        if RECORD_VIDEO:
-            RECORD_DIR = os.path.join(f"{RECORD_BASE_DIR}/{result_dir}/iter{iter}/idx{idx}")
-            command += f" enable_recording=True record_dir={RECORD_DIR}"
         if not isinstance(iter, int):
             iter = int(iter.split("iter")[1])
-        if not os.path.exists(f"{result_dir}/iter{iter}/{idx}/nn") or not os.listdir(f"{result_dir}/iter{iter}/{idx}/nn"):
-            raise FileNotFoundError(f"{result_dir}/iter{iter}/{idx}/nn")         
-        CHECKPOINT_NAME = sorted([x for x in os.listdir(f"{result_dir}/iter{iter}/{idx}/nn") if os.path.isfile(os.path.join(f"{result_dir}/iter{iter}/{idx}/nn", x))], key=lambda x: os.path.getmtime(f"{result_dir}/iter{iter}/{idx}/nn/{x}"))[-1]
+        if RECORD_VIDEO:
+            RECORD_DIR = os.path.join(
+                current_dir, f"{RECORD_BASE_DIR}/{result_dir}/iter{iter}/idx{idx}"
+            )
+            command += f" enable_recording=True recording_dir={RECORD_DIR}"
+        if not os.path.exists(f"{result_dir}/iter{iter}/{idx}/nn") or not os.listdir(
+            f"{result_dir}/iter{iter}/{idx}/nn"
+        ):
+            raise FileNotFoundError(f"{result_dir}/iter{iter}/{idx}/nn")
+        CHECKPOINT_NAME = sorted(
+            [
+                x
+                for x in os.listdir(f"{result_dir}/iter{iter}/{idx}/nn")
+                if os.path.isfile(os.path.join(f"{result_dir}/iter{iter}/{idx}/nn", x))
+            ],
+            key=lambda x: os.path.getmtime(f"{result_dir}/iter{iter}/{idx}/nn/{x}"),
+        )[-1]
         code_path = f"{result_dir}/iter{iter}/{idx}/env_iter{iter}_response{idx}.py"
         shutil.copyfile(code_path, TASK_NAME_PATH)
-        checkpoint_path = f"{current_dir}/{result_dir}/iter{iter}/{idx}/nn/{CHECKPOINT_NAME}"
+        checkpoint_path = (
+            f"{current_dir}/{result_dir}/iter{iter}/{idx}/nn/{CHECKPOINT_NAME}"
+        )
+        command += f" checkpoint={checkpoint_path}"
         if not os.path.exists(checkpoint_path):
             raise FileNotFoundError(checkpoint_path)
         logger.info(f"Executing command: {command}")
         process = subprocess.Popen(command, shell=True)
         process.wait()
         logger.info("Execution completed!")
-    
+
     if RUN_ALL:
         # Run all the checkpoints under RESULT_DIR
-        for iter in sorted([x for x in os.listdir(RESULT_DIR) if os.path.isdir(os.path.join(RESULT_DIR, x))]):
-            for idx in sorted([x for x in os.listdir(f"{RESULT_DIR}/{iter}") if os.path.isdir(os.path.join(f"{RESULT_DIR}/{iter}", x))]):
+        for iter in sorted(
+            [
+                x
+                for x in os.listdir(RESULT_DIR)
+                if os.path.isdir(os.path.join(RESULT_DIR, x))
+            ]
+        ):
+            for idx in sorted(
+                [
+                    x
+                    for x in os.listdir(f"{RESULT_DIR}/{iter}")
+                    if os.path.isdir(os.path.join(f"{RESULT_DIR}/{iter}", x))
+                ]
+            ):
                 logger.info(f"Running for iter: {iter} and idx: {idx}")
                 try:
                     run_one(RESULT_DIR, iter, idx, command)
                 except FileNotFoundError as e:
-                    logger.warning(f"Iter: {iter} Index: {idx} doesn't not contain a checkpoint: {e}")
+                    logger.warning(
+                        f"Iter: {iter} Index: {idx} doesn't not contain a checkpoint: {e}"
+                    )
                     continue
                 except Exception as e:
                     logger.error(f"Error occurred: {e}")
@@ -86,11 +121,12 @@ def main(cfg: DictConfig):
         try:
             run_one(RESULT_DIR, ITER, IDX, command)
         except FileNotFoundError as e:
-            logger.warning(f"Iter: {ITER} Index: {IDX} doesn't not contain a checkpoint: {e}")
+            logger.warning(
+                f"Iter: {ITER} Index: {IDX} doesn't not contain a checkpoint: {e}"
+            )
         except Exception as e:
             logger.error(f"Error occurred: {e}")
 
-        
 
 if __name__ == "__main__":
     main()
